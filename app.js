321342ded3356135bdcef80ba5c8f305245be6b7
Zoe <zoebrambilla123@gmail.com>
2026-04-09 11:43:48 -0300

    Rutas definidas

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 //instanciamos la app
 const app = express();
 
+//decimos en donde van a estar los estilos a usar
+app.use(express.static("assets"));
+
 const PORT = 3000;
 
 //ruta raíz
@@ -11,6 +14,28 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("pages/index")
 })
+
+//ur2 rutas
+
+app.get("/cart", (req, res) => {
+    res.render("pages/cart");
+});
+
+app.get("/checkout", (req, res) => {
+    res.render("pages/checkout");
+});
+
+app.get("/login", (req, res) => {
+    res.render("pages/login");
+});
+
+app.get("/product", (req, res) => {
+    res.render("pages/product");
+});
+
+app.get("/register", (req, res) => {
+    res.render("pages/register");
+});
 
 //iniciar el servidor
 app.listen(PORT, () => {
