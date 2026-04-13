8064f74aaf3d9d0a65342e17a8916d40b3ebc154
Zoe <zoebrambilla123@gmail.com>
2026-04-13 12:04:49 -0300

    lógica para el carrito

--- app.js
+++ app.js
@@ -14,27 +14,73 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 
-
-//ur2 rutas
-
-app.get("/cart", (req, res) => {
-    res.render("pages/cart");
-});
-
-app.get("/checkout", (req, res) => {
-    res.render("pages/checkout");
-});
-
 //iniciar el servidor
 app.listen(port, () => {
     console.log(`App funcionando en el puerto ${port}`)
 });
+
+//DATOS
+//array de los productos (ahora estan aca, pueden ir en un archivo json)
+const misProductos = 
+[
+    {
+        id: 1, 
+        nombre: "whiskey Jack Daniels Honey 750ml", 
+        precio: 9500, 
+        descripcion: "Descubrí el sabor único de Jack Daniels Honey, una combinación perfecta de whiskey y miel que deleitará tu paladar. Su suave dulzura y notas de vainilla lo convierten en la elección ideal para disfrutar solo o en cocktails innovadores. Con su presentación elegante es el regalo perfecto para compartir en ocasiones especiales o simplemente para darte un gusto. ¡No esperes más! Comprá ahora y llevá el sabor inconfundible de Jack Daniels Honey a tu hogar.", 
+        categoria: "Bebidas", 
+        imagen: "/img/whiskey.jpg"
+    }, 
+    {
+        id: 2, 
+        nombre: "Hamburguesa completa", 
+        precio: 3000, 
+        descripcion: "Hamburguesa de carne 100% vacuna  con jamón, queso, tomate y lechuga manteca. Un clásico para compartir en todo momento.", 
+        categoria: "Comidas", 
+        imagen: "/img/hamburguesa.jpg" 
+    },  
+    {
+        id: 3, 
+        nombre: "Chenin dulce Santa Julia", 
+        precio: 1500, 
+        descripcion: "Santa Julia Chenin Dulce Natural es un vino elaborado con uvas de la variedad Chenin Blanc. Es un vino suave y delicado, de color amarillo verdoso y aromas que recuerdan a durazno blanco, damasco, hierbas frescas y algunasnotas cítricas como limón y pomelo.Santa Julia Chenin Dulce Natural resulta ideal como aperitivo o bien para acompañar picadas, frutos de mar o postres con frutas frescas y cítricas." , 
+        imagen: "/img/vino.jpg", 
+        categoria: "Bebidas"
+    },
+    {id: 4, nombre: "Wrap de ensalada caesar", precio: 1800, descripcion: "El wrap de ensalada César es una opción práctica y sabrosa que envuelve la clásica ensalada en una tortilla de harina. Combina lechuga romana crujiente, pollo (asado o empanado), queso parmesano y aderezo César cremoso, croutones para aportar textura. Es una opción de comida rápida, portátil y versátil.", imagen: "/img/wrap.jpg", categoria: "Comidas"},
+    {id: 5, nombre: "Caipiriña", precio: 1000, descripcion: "Caipiriña es una bebida brasileña, originaria del estado de São Paulo. Clasificada como un cóctel, su ingrediente principal es la cachaza, pero también lleva lima o limón, azúcar y hielo.", imagen: "/img/caipiriña.jpg", categoria: "Cocteles"},
+    {id: 6, nombre: "Papas Fritas con cheddar", precio: 2000, descripcion: "Calientes, crujientes y deliciosas, una nueva variedad llega para quedarse: Papas Fritas con Cheddar fundido.", imagen: "/img/papas.jpg", categoria: "Comidas"}
+]
 
 //array de usuarios
 const usuario = [
     {nombreU: "Lucia", email: "lucia123@email.com", password: "lucia1234"}
 ];
 
+let cart = [];
+
+
+//us2 rutas
+
+//CHECKOUT
+app.get("/checkout", (req, res) => {
+    res.render("pages/checkout");
+});
+
+//INDEX
+app.get("/", (req, res) => {
+    //Extraer categorias
+    const cat = misProductos.map(p => p.categoria);
+    //para que no haya repetidos (set) y convertir en array
+    const categoriasBarra = [...new Set(cat)];
+
+    res.render("pages/index", {
+        productos: misProductos,
+        categorias: categoriasBarra
+    });
+});
+
+//REGISTER
 app.get("/register", (req, res) => {  
     res.render("pages/register");
 });
@@ -50,6 +96,8 @@ app.post("/register", (req, res) => {
     usuario.push(nuevoUsuario);
     res.redirect("/login"); 
 });
+
+//LOGIN
 app.get("/login", (req, res) => {
     res.render("pages/login");
 });
@@ -62,49 +110,32 @@ app.post("/login", (req, res) => {
     }
 });
 
+//CARRITO
 
-//array de los productos (ahora estan aca, pueden ir en un archivo json)
-const misProductos = 
-[
-    {id: 1, nombre: "whiskey Jack Daniels Honey 750ml", precio: 9500, descripcion: "Descubrí el sabor único de Jack Daniels Honey, una combinación perfecta de whiskey y miel que deleitará tu paladar. Su suave dulzura y notas de vainilla lo convierten en la elección ideal para disfrutar solo o en cocktails innovadores. Con su presentación elegante es el regalo perfecto para compartir en ocasiones especiales o simplemente para darte un gusto. ¡No esperes más! Comprá ahora y llevá el sabor inconfundible de Jack Daniels Honey a tu hogar.", categoria: "Bebidas", imagen: "/public/img/whiskey.jpg"}, 
-    {id: 2, nombre: "Hamburguesa completa", precio: 3000, descripcion: "Hamburguesa de carne 100% vacuna  con jamón, queso, tomate y lechuga manteca. Un clásico para compartir en todo momento.", categoria: "Comidas", imagen: "/public/img/hamburguesa.jpg" },  
-    {id: 3, nombre: "Chenin dulce Santa Julia", precio: 1500, descripcion: "Santa Julia Chenin Dulce Natural es un vino elaborado con uvas de la variedad Chenin Blanc. Es un vino suave y delicado, de color amarillo verdoso y aromas que recuerdan a durazno blanco, damasco, hierbas frescas y algunasnotas cítricas como limón y pomelo.Santa Julia Chenin Dulce Natural resulta ideal como aperitivo o bien para acompañar picadas, frutos de mar o postres con frutas frescas y cítricas." , imagen: "/public/img/vino.jpg", categoria: "Bebidas"},
-    {id: 4, nombre: "Wrap de ensalada caesar", precio: 1800, descripcion: "El wrap de ensalada César es una opción práctica y sabrosa que envuelve la clásica ensalada en una tortilla de harina. Combina lechuga romana crujiente, pollo (asado o empanado), queso parmesano y aderezo César cremoso, croutones para aportar textura. Es una opción de comida rápida, portátil y versátil.", imagen: "/public/img/wrap.jpg", categoria: "Comidas"},
-    {id: 5, nombre: "Caipiriña", precio: 1000, descripcion: "Caipiriña es una bebida brasileña, originaria del estado de São Paulo. Clasificada como un cóctel, su ingrediente principal es la cachaza, pero también lleva lima o limón, azúcar y hielo.", imagen: "/public/img/caipiriña.jpg", categoria: "Cocteles"},
-    {id: 6, nombre: "Papas Fritas con cheddar", precio: 2000, descripcion: "Calientes, crujientes y deliciosas, una nueva variedad llega para quedarse: Papas Fritas con Cheddar fundido.", imagen: "/public/img/papas.jpg", categoria: "Comidas"}
-]
-//hay que enviarlo al index (ruta raíz)
+let porcentajeDescuento = 0; // Se actualiza cuando alguien usa la ruta /cart/coupon
 
-app.get("/", (req, res) => {
-    //Extraer categorias
-    const cat = misProductos.map(p => p.categoria);
-    //para que no haya repetidos (set) y convertir en array
-    const categoriasBarra = [...new Set(cat)];
+app.get("/cart", (req, res) => {
+    //Calculamos el subtotal dinámicamente cada vez que se carga la página
+    let subtotal = cart.reduce((acumulador, producto) => {
+        return acumulador + (producto.precio * producto.quantity);
+    }, 0);
 
-    res.render("pages/index", {
-        productos: misProductos,
-        categorias: categoriasBarra
+    //Calculamos cuánto es el descuento en dinero
+    let descuentoPesos = subtotal * (porcentajeDescuento / 100);
+
+    //El total final que va a pagar
+    let totalFinal = subtotal - descuentoPesos;
+
+    //Enviamos todo a la vista
+    res.render("pages/cart", {
+        cart: cart,
+        total: totalFinal,
+        descuento: porcentajeDescuento,
+        subtotal: subtotal 
     });
 });
 
-//cuando se toca una categoria, manda al index pero filtrado
-
-app.get("/productos/categoria/:nombre", (req, res) => {
-    const categoriaElegida = req.params.nombre;
-
-    const productosFiltrados = misProductos.filter(p => p.categoria == categoriaElegida);
-
-    //Generamos de nuevo la lista para la barra (para que no desaparezca)
-    const cat = misProductos.map(p => p.categoria);
-    const categoriasBarra = [...new Set(cat)];
-
-    //Reutilizamos el index.ejs
-    res.render("pages/index", {
-        productos: productosFiltrados,
-        categorias: categoriasBarra
-    });
-});
-
+//PRODUCTO
 //us5 Detalle de un pedido especifico
 app.get("/product/:id", (req,res) => {
     idProducto = req.params.id; //busca lo variable de la ulr
@@ -130,7 +161,68 @@ app.get("/product/:id", (req,res) => {
     }
 });
 
+//cuando se toca una categoria, manda al index pero filtrado
+
+app.get("/productos/categoria/:nombre", (req, res) => {
+    const categoriaElegida = req.params.nombre;
+
+    const productosFiltrados = misProductos.filter(p => p.categoria == categoriaElegida);
+
+    //Generamos de nuevo la lista para la barra (para que no desaparezca)
+    const cat = misProductos.map(p => p.categoria);
+    const categoriasBarra = [...new Set(cat)];
+
+    //Reutilizamos el index.ejs
+    res.render("pages/index", {
+        productos: productosFiltrados,
+        categorias: categoriasBarra
+    });
+});
+
+
+
 //funcion para ir agregando al carrito
 
+app.post("/agregar-carrito", (req, res) => {
+const id = req.body.idProducto;
 
+//buscar producto
+const productoCarrito = misProductos.find(p => p.id == id);
 
+if (productoCarrito) {
+        productoCarrito.quantity = 1;
+        cart.push(productoCarrito);
+        console.log("Carrito actual:", cart); // Esto para ver en la terminal si se guarda
+        
+        // Redirigimos a la misma página agregando ?success=true a la URL
+        res.redirect(`/product/${id}?success=true`);
+    } else {
+        res.status(404).send("No se pudo agregar el producto");
+    }
+})
+
+// RUTA PARA AUMENTAR
+app.post("/cart/increase/:id", (req, res) => {
+    const id = req.params.id;
+    const producto = cart.find(p => p.id == id);
+
+    if (producto) {
+        producto.quantity += 1;
+    }
+    res.redirect("/cart");
+});
+
+// RUTA PARA DISMINUIR
+app.post("/cart/decrease/:id", (req, res) => {
+    const id = req.params.id;
+    const producto = cart.find(p => p.id == id);
+
+    if (producto) {
+        if (producto.quantity > 1) {
+            producto.quantity -= 1;
+        } else {
+            cart = cart.filter(p => p.id != id);
+        }
+    }
+    res.redirect("/cart");
+});
